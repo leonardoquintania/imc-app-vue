1971b51ddc185b05d03e06b0366f82e2d86b76af
Leonardo dos Santos Quintania <leonardo.quintania@totvs.com.br>
2021-04-18 22:16:22 -0300

    Aulas IGTI VUE

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="label-title">Calculo do IMC</h1>
+  <h2 class="label-subtitle">Digite seu peso e altura para calular o IMC</h2>
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText
+        id="input-weight"
+        type="text"
+        v-model="weight"
+        :disabled="imc"
+      />
+      <label for="input-weight">Peso</label>
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText
+        id="input-height"
+        type="text"
+        v-model="height"
+        :disabled="imc"
+      />
+      <label for="input-height">Altura</label>
+    </span>
+  </div>
+  <div class="div-imc" v-if="!imc">
+    <Button label="Limpar" @click="clear" />
+    <Button style="margin-left: 1rem " label="Calcular" @click="calculate" />
+  </div>
+  <div class="div-imc" v-if="imc">
+    <p class="label-result">Seu IMC é: {{ imc }}</p>
+    <p class="label-classification">
+      A classificação do seu IMC é: {{ classification }}
+    </p>
+    <Button label="Calcular novamente" @click="clear" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      height: null,
+      weight: null,
+      imc: null,
+      classification: '',
+    };
+  },
+  methods: {
+    calculate: function() {
+      this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+      this.classification = this.defineClassification(this.imc);
+    },
+    defineClassification(imc) {
+      if (imc < 18.5) {
+        return 'Magreza';
+      } else if (imc >= 18.5 && imc < 25) {
+        return 'Normal';
+      } else if (imc >= 25 && imc < 30) {
+        return 'Sobrepeso';
+      } else if (imc >= 30 && imc < 40) {
+        return 'Obesidade';
+      } else {
+        return 'Obesidade grave';
+      }
+    },
+    clear() {
+      this.height = null;
+      this.weight = null;
+      this.imc = null;
+      this.classification = '';
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +80,30 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.div-imc {
+  margin-top: 2rem;
+}
+
+.label-title {
+  font-size: 2rem;
+}
+
+.label-subtitle {
+  font-size: 1.1rem;
+}
+
+.label-result {
+  font-size: 2rem;
+}
+
+.label-classification {
+  font-size: 1.5rem;
+}
+
+button {
+  margin-right: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component('InputText', InputText);
+app.component('Button', Button);
+
+app.mount('#app');
